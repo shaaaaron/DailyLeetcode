b5d197fb87500571e3d70dc348f88d00c3c1d549
yu zhou <1750914946@qq.com>
2024-03-26 21:58:19 +0800

    clean code

---
+++ 375.猜数字大小-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=375 lang=cpp
+ *
+ * [375] 猜数字大小 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+
+    }
+};
+// @lc code=end
+
